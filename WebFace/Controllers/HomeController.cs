41afae90a17d0e1d78f88292a01fb06dd9244e2f
Pavel <pho@2021.ai>
2019-01-28 10:03:02 +0100

    upload working and detecting

---
+++ WebFace/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WebFace.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"),
+                    fileName ?? throw new InvalidOperationException());
+
+                file.SaveAs(path);
+
+                var result = Convert(fileName);
+            }
+            else
+            {
+                // print error msg
+            }
+
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Pokus()
+        {
+
+            var ret = Convert("skoupil.jpg");
+
+            return Json(ret, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult Skupina()
+        {
+
+            var ret = Convert("skupina.jpg");
+
+            return Json(ret, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult None()
+        {
+
+            var ret = Convert("dom.jpg");
+
+            return Json(ret, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        private Rectangle[] Convert(string name)
+        {
+            var bmp = (Bitmap) Image.FromFile(Server.MapPath("~/App_Data/uploads/" + name));
+
+            var ret = Analyze(bmp);
+
+
+            var img2 = new Bitmap(bmp);
+
+            Graphics g = Graphics.FromImage(img2);
+            foreach (var rectangle in ret)
+            {
+                g.FillRectangle(new SolidBrush(Color.FromArgb(50, 255, 0, 0)), rectangle);
+            }
+
+
+            g.Save();
+            img2.Save(Server.MapPath("~/App_Data/uploads/x_" + name));
+            return ret;
+        }
+
+
+        private Rectangle[] Analyze(Bitmap bmp)
+        {
+            //Emgu.CV.ICapture capture = new VideoCapture();
+
+            //var cap = capture.QueryFrame();
+            Image<Rgb, Byte> x = new Image<Rgb, Byte>(bmp); //cap.ToImage<Rgb, DepthType>();
+            var img = x.Convert<Emgu.CV.Structure.Gray, Byte>();
+
+
+            var cascadeClassifier =
+                new CascadeClassifier(Server.MapPath("~/App_Data/" + "/haarcascade_frontalface_default.xml"));
+
+            using (var imageFrame = x)
+            {
+                if (imageFrame != null)
+                {
+                    var grayframe = imageFrame.Convert<Gray, Byte>();
+                    var faces = cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10,
+                        Size.Empty); //the actual face detection happens here
+                    return faces;
+                }
+
+            }
+
+            return new Rectangle[0];
+        }
+    }
+}
